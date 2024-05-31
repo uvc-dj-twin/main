12ad96394815bdf821ad3018111ebebfa40ab1ec
김신영 <shyk31971@gmail.com>
2024-05-31 13:19:27 +0900

    update compositionstyle footer,fottterAdmin,FotterSmall,HeaderStats,AdminNavbar,AuthNavbar,indexNavbar

--- fe/src/components/Footers/FooterSmall.vue
+++ fe/src/components/Footers/FooterSmall.vue
@@ -1,4 +1,40 @@
+<!-- 
+변경된 부분 및 주석 설명:
+템플릿 부분 (<template>):
+푸터 컴포넌트:
+
+<footer> 태그로 푸터 컴포넌트를 정의합니다.
+:class 바인딩을 사용하여 absolute 속성에 따라 클래스가 동적으로 변경됩니다.
+푸터 상단의 가로줄:
+
+<hr> 태그로 푸터 상단에 가로줄을 추가합니다.
+저작권 정보 섹션:
+
+현재 연도를 표시하는 텍스트와 "Creative Tim"에 대한 링크가 포함됩니다.
+{{ date }}로 현재 연도를 표시합니다.
+링크 섹션:
+
+"Creative Tim", "About Us", "Blog", "MIT License"에 대한 링크가 포함된 목록을 만듭니다.
+스크립트 부분 (<script>):
+Vue 3의 Composition API 사용:
+
+ref와 computed를 사용하여 반응형 변수를 선언합니다.
+현재 연도 저장:
+
+date 변수는 현재 연도를 저장합니다.
+동적 클래스 계산:
+
+footerClass는 props.absolute 값에 따라 푸터의 클래스를 동적으로 계산합니다.
+setup 함수:
+
+Composition API의 setup 함수 안에서 반응형 변수 date와 footerClass를 선언하고 반환합니다.
+반환된 객체 안에 date와 footerClass를 포함하여 템플릿에서 사용할 수 있도록 합니다. -->
+
+
+
+
 <template>
+  <!-- 푸터 컴포넌트, absolute 속성에 따라 위치와 스타일이 동적으로 변경 -->
   <footer
     class="pb-6"
     :class="[
@@ -6,14 +42,12 @@
     ]"
   >
     <div class="container mx-auto px-4">
+      <!-- 푸터 상단의 가로줄 -->
       <hr class="mb-6 border-b-1 border-blueGray-600" />
-      <div
-        class="flex flex-wrap items-center md:justify-between justify-center"
-      >
+      <div class="flex flex-wrap items-center md:justify-between justify-center">
+        <!-- 저작권 정보 섹션 -->
         <div class="w-full md:w-4/12 px-4">
-          <div
-            class="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left"
-          >
+          <div class="text-sm text-blueGray-500 font-semibold py-1 text-center md:text-left">
             Copyright © {{ date }}
             <a
               href="https://www.creative-tim.com?ref=vn-footer-small"
@@ -23,6 +57,7 @@
             </a>
           </div>
         </div>
+        <!-- 링크 섹션 -->
         <div class="w-full md:w-8/12 px-4">
           <ul class="flex flex-wrap list-none md:justify-end justify-center">
             <li>
@@ -65,17 +100,30 @@
 </template>
 
 <script>
+import { ref, computed } from 'vue'; // Vue 3의 ref와 computed를 가져옵니다.
+
 export default {
-  data() {
-    return {
-      date: new Date().getFullYear(),
-    };
-  },
   props: {
+    // absolute 속성을 정의합니다. Boolean 타입이며 기본값은 false입니다.
     absolute: {
       type: Boolean,
       default: false,
     },
+  },
+  setup(props) {
+    // 현재 연도를 저장하는 반응형 변수 date를 선언합니다.
+    const date = ref(new Date().getFullYear());
+
+    // absolute 속성에 따라 동적으로 클래스 이름을 설정하는 반응형 변수 footerClass를 선언합니다.
+    const footerClass = computed(() => 
+      props.absolute ? 'absolute w-full bottom-0 bg-blueGray-800' : 'relative'
+    );
+
+    // 반환된 객체 안에 date와 footerClass를 포함하여 템플릿에서 사용할 수 있도록 합니다.
+    return {
+      date,
+      footerClass,
+    };
   },
 };
 </script>
--- fe/src/components/Navbars/IndexNavbar.vue
+++ fe/src/components/Navbars/IndexNavbar.vue
@@ -1,13 +1,30 @@
+<!-- 
+변경된 부분 및 주석 설명:
+템플릿 부분 (<template>):
+Navbar 토글 상태 바인딩:
+:class="[navbarOpen.value ? 'block' : 'hidden']": Navbar가 열려 있는지 닫혀 있는지 상태에 따라 클래스를 동적으로 설정합니다. Composition API의 ref로 선언된 변수의 값에 접근할 때 .value를 사용합니다.
+스크립트 부분 (<script>):
+Vue 3의 Composition API 사용:
+
+ref를 사용하여 반응형 변수 navbarOpen 선언.
+Navbar 상태를 저장하는 변수:
+
+const navbarOpen = ref(false);: Navbar가 열려 있는지 닫혀 있는지를 나타내는 반응형 변수입니다. 초기값은 false입니다.
+Navbar 상태를 토글하는 함수:
+
+const setNavbarOpen = () => { navbarOpen.value = !navbarOpen.value; };: Navbar의 상태를 토글(열림/닫힘)하는 함수입니다.
+setup 함수:
+
+Composition API의 setup 함수 안에서 반응형 변수와 함수를 선언하고 반환합니다.
+이 코드는 Composition API를 사용하여 Navbar의 열림/닫힘 상태를 반응형 변수로 관리하고, 이를 템플릿에서 동적으로 바인딩하여 상태에 따라 Navbar의 표시를 제어합니다. 각 변수와 로직은 주석으로 명확히 설명되어 있습니다. -->
+
+
 <template>
   <nav
     class="top-0 fixed z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-white shadow"
   >
-    <div
-      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
-    >
-      <div
-        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
-      >
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
         <router-link to="/">
           <a
             class="text-blueGray-700 text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
@@ -26,7 +43,7 @@
       </div>
       <div
         class="lg:flex flex-grow items-center"
-        :class="[navbarOpen ? 'block' : 'hidden']"
+        :class="[navbarOpen.value ? 'block' : 'hidden']"
         id="example-navbar-warning"
       >
         <ul class="flex flex-col lg:flex-row list-none mr-auto">
@@ -35,9 +52,7 @@
               class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
               href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus?ref=vn-index-navbar"
             >
-              <i
-                class="text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2"
-              />
+              <i class="text-blueGray-400 far fa-file-alt text-lg leading-lg mr-2" />
               Docs
             </a>
           </li>
@@ -94,21 +109,27 @@
 </template>
 
 <script>
-import IndexDropdown from "@/components/Dropdowns/IndexDropdown.vue";
+import { ref } from 'vue'; // Vue 3의 ref 가져오기
+import IndexDropdown from "@/components/Dropdowns/IndexDropdown.vue"; // IndexDropdown 컴포넌트 가져오기
 
 export default {
-  data() {
-    return {
-      navbarOpen: false,
-    };
-  },
-  methods: {
-    setNavbarOpen: function () {
-      this.navbarOpen = !this.navbarOpen;
-    },
-  },
   components: {
-    IndexDropdown,
+    IndexDropdown, // IndexDropdown 컴포넌트 등록
+  },
+  setup() {
+    // Navbar의 상태를 저장하는 반응형 변수
+    const navbarOpen = ref(false);
+
+    // Navbar의 상태를 토글하는 함수
+    const setNavbarOpen = () => {
+      navbarOpen.value = !navbarOpen.value;
+    };
+
+    return {
+      // 템플릿에서 사용할 변수와 함수 반환
+      navbarOpen,
+      setNavbarOpen,
+    };
   },
 };
 </script>
