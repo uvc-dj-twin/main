a53fd29b9720ef05c01d7d863b2438025d78193d
copeve1015 <copeve1015@gmail.com>
2024-06-12 11:26:36 +0900

    fix send bug

--- be/error/ErrorHandler.js
+++ be/error/ErrorHandler.js
@@ -3,7 +3,7 @@ const logger = require('../lib/logger');
 const errorHandler = (err, req, res, next) => {
   logger.error(err.message);
   const statusCode = err.statusCode || 500; // 기본 상태 코드를 500으로 설정
-  res.status(statusCode).json({ message: err.message }).send();
+  res.status(statusCode).json({ message: err.message });
 };
 
 module.exports = errorHandler;
