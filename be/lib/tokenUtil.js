1da2d028fed0d826973c355863c814f91d612522
copeve1015 <copeve1015@gmail.com>
2024-06-17 16:58:46 +0900

    refresh token 발급

--- be/lib/tokenUtil.js
+++ be/lib/tokenUtil.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const secretKey = process.env.TOKEN_SECRET || 'fc64e3e9f7dafdbd937862566087d921';
+const secretKey = process.env.TOKEN_SECRET || 'TB5E1sbMDFbEb1KhFyGAM10HjgTsz5mC';
+const refreshSecretKey = process.env.REFRESH_TOKEN_SECRET || 'TB5E1sbMDFbEb1KhFyGAM10HjgTsz5mC';
 
 const options = {
   expiresIn: process.env.TOKEN_EXPIRESIN || '2h',
 };
+const refreshOptions = {
+  expiresIn: process.env.REFRESH_TOKEN_EXPIRESIN || '14d',
+}
 
 const tokenUtil = {
   makeToken(user) {
@@ -24,7 +28,25 @@ const tokenUtil = {
     } catch (err) {
       return null;
     }
-  }
+  },
+  makeRefreshToken(user) {
+    const payload = {
+      id: user.id,
+      name: user.name,
+      role: user.role,
+    };
+    const token = jwt.sign(payload, refreshSecretKey, refreshOptions);
+    
+    return token;
+  },
+  verifyRefreshToken(token) {
+    try {
+      const decoded = jwt.verify(token, refreshSecretKey);
+      return decoded;
+    } catch (err) {
+      return null;
+    }
+  },
 };
 
 module.exports = tokenUtil;
