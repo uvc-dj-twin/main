b48417e5de925259291caab5d9054fcdabaff0cc
copeve1015 <copeve1015@gmail.com>
2024-06-20 21:00:04 +0900

    update CardBarChart.vue, PieChart.vue, Details.vue and Statistics.vue

--- fe/src/views/dataread/Statistics.vue
+++ fe/src/views/dataread/Statistics.vue
@@ -8,7 +8,7 @@
 
         <div style="display:flex">
           <HeaderDataRead />
-          <div class="flex items-center justify-end w-full h-full" style="gap:16px">
+          <div class="flex items-center justify-end w-full h-full" style="gap:16px; height: 65px;">
 
             <EquipmentDropdown :equipmentList="equipmentList" :value="selectedValue" @update:value="handleUpdate">
             </EquipmentDropdown>
@@ -20,8 +20,8 @@
 
             </div>
             <button @click="getValue"
-              class="text-2xl px-4 get-started text-white font-bold rounded outline-none focus:outline-none mr-1 mb-1 bg-color1 active:bg-emerald-600"
-              style="height:32px" type="button" click="">
+              class="text-xl px-4 py-1 get-started  font-bold rounded outline-none focus:outline-none mr-1 mb-1 bg-color1 active:bg-color1 bg-white border-2 border-blueGray-600 text-black active:bg-emerald-600"
+              style="line-height: 20px; height:32px" type="button" click="">
               조회
             </button>
           </div>
